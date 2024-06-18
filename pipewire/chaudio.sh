f535ca42911b6440e56fb6a9da178095752fa498
Yadav Lamichhane <omegazyadav1@gmail.com>
2024-06-18 08:56:31 +0545

    fix: use script to change the default sink

--- pipewire/chaudio.sh
+++ pipewire/chaudio.sh
@@ -1,19 +1,31 @@
-#!/bin/bash
+##!/bin/bash
 
-# Step 1: List the available sinks and find the one for the system speaker
-speaker_sink=$(pactl list short sinks | grep -i 'alsa_output' | awk '{print $2}')
+# Get the list of sinks
+sinks=$(pactl list short sinks | awk '{print $1}')
 
-# Step 2: Set the default sink to the system speaker sink
-if [ -n "$speaker_sink" ]; then
-  pactl set-default-sink "$speaker_sink"
-  echo "Default sink set to $speaker_sink"
+# Get the current default sink
+current_sink=$(pactl list short sinks | grep -i 'RUNNING' | awk '{print $1}')
 
-  # Step 3: Move any currently playing audio streams to the new default sink
-  for stream in $(pactl list short sink-inputs | awk '{print $1}'); do
-    pactl move-sink-input "$stream" "$speaker_sink"
-  done
-  echo "Moved all audio streams to $speaker_sink"
-else
-  echo "No system speaker sink found"
-fi
+# Convert the list of sinks into an array
+sink_array=($sinks)
+# Find the index of the current sink in the array
+current_index=-1
+for i in "${!sink_array[@]}"; do
+    if [[ "${sink_array[$i]}" == "$current_sink" ]]; then
+        current_index=$i
+        break
+    fi
+done
 
+# Get the next sink in the list
+next_index=$(( (current_index + 1) % ${#sink_array[@]} ))
+next_sink=${sink_array[$next_index]}
+
+# Set the next sink as the default
+pactl set-default-sink $next_sink
+
+# Move all current audio streams to the new sink
+pactl list short sink-inputs | while read -r stream; do
+    stream_id=$(echo $stream | awk '{print $1}')
+    pactl move-sink-input $stream_id $next_sink
+done
